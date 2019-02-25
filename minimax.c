2c75b8d520ba321ce60cd55eea8a2e4ee5941535
MDAddario <38199062+MDAddario@users.noreply.github.com>
2019-02-24 20:13:28 -0500

    Justified half turn metric (HTM)

--- minimax.c
+++ minimax.c
@@ -17,7 +17,7 @@ int minimax(rubiks* cube, int depth){
 	int value, old_last_move;
 	void (*move_array[6]) (rubiks* cube, int count) = {U, R, F, D, L, B};
 
-	// Consider all possible moves
+	// Consider all possible moves in HTM
 	for (int move = 0; move < 6; move++){
 
 		// Prevent rotating the same face consecutively
@@ -51,3 +51,31 @@ int minimax(rubiks* cube, int depth){
 	}
 	return value;
 }
+
+/*	
+ *	Is it more economic to analyze all tree nodes in half turn metric (HTM) or quarter turn metric (QTM)?
+ *	The maximum depth of the minimax algorithm will be God's number (https://cube20.org/). 
+ *
+ *	In HTM, God's number is 20
+ *	At each level, 3 moves on each face can be made (R, R2, R')
+ *	Beyond the first move, it is prohibited to consecutively turn the same face
+ *	i.e. R -/> R, R2, R'
+ *	Thus, the total number of positions to run is:
+ *	N_{htm} = (3*6) * (3*6 - 3) ^ 19 
+ *	
+ *	In QTM, God's number is 26
+ *	At each level, 2 moves on each face can be made (R, R')
+ *	Beyond the first move, it is prohibited to perform the inverse move
+ *	i.e. R -/> R', but R -> R is permitted
+ *	Thus, the total number of positions to ru is:
+ *	N_{qtm} = (2*6) * (2*6 - 1) ^ 25
+ *
+ *	Which of these numbers is greater?
+ *	N_{qtm} / N_{htm} ~= 3258
+ *
+ *	Thus, there are many less positions to evaluate in HTM, so we use HTM.
+ *
+ *	(Yes, this was crude. In QTM, after two consecutive R's, there should be no R or R'.
+ *	In addition, for QTM and HTM this calculation doesn't account for cutting out 
+ *	different permutation orders for turning different faces)
+ */
